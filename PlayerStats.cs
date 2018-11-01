f2c3a80ee1cced3fada0fc7b6ad3bccfe2663fc3
WEST <mcw3@cocsce-l1d39-07.ds.sc.edu>
2018-11-01 16:03:28 -0400

    scripts added

---
+++ PlayerStats.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+    public float health = 100f;
+
+    public float ecto = 100f;
+}
---
+++ Power.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Power : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine( Pickup(other) ); 
+        }
+    }
+
+    IEnumerator Pickup(Collider player)
+    {
+        PlayerStats stats = player.GetComponent<PlayerStats>();
+        CapsuleCollider collide = player.GetComponent<CapsuleCollider>();
+        if (stats.ecto >= 10)
+        {
+            // Hypothetically this should disable the collider so player can go through walls... it does not work tho
+            collide.isTrigger = true;
+            // For now health boost is just to make sure it's working
+            stats.health *= 1.2f;
+            stats.ecto -= 10;
+
+            yield return new WaitForSeconds(4f);
+
+            stats.health /= 1.2f;
+            collide.isTrigger = false;
+
+            Destroy(gameObject);
+        }
+    }
+}
